6d9fc90d24906a8f93dc8fc72ea7ef3b1306bcd5
Rostyslav Sokalskyi <rostyksokalskyi@gmail.com>
2025-06-21 18:46:52 +0300

    feat: add navigations

--- CurrencyTracker/src/navigation/AppNavigator.tsx
+++ CurrencyTracker/src/navigation/AppNavigator.tsx
@@ -1,3 +1,68 @@
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import AllСurrenciesScreen from "../screens/AllСurrenciesScreen";
+import FavoritesScreen from "../screens/FavoritesScreen";
+import SettingsScreen from "../screens/SettingsScreen";
+
+export type RootStackParamList = {
+  HomeTabs: undefined;
+  Settings: undefined;
+};
+
+const Stack = createStackNavigator<RootStackParamList>();
+const Tab = createBottomTabNavigator();
+
+function HomeTabs() {
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: true,
+        headerTitleAlign: "center",
+      }}
+    >
+      <Tab.Screen
+        name="AllCurrenciesTab"
+        component={AllСurrenciesScreen}
+        options={{ title: "All Rates" }}
+      />
+
+      <Tab.Screen
+        name="FavoritesTab"
+        component={FavoritesScreen}
+        options={{ title: "Favorites" }}
+      />
+
+      <Tab.Screen
+        name="SettingsTab"
+        component={SettingsScreen}
+        options={{ title: "Settings" }}
+      />
+    </Tab.Navigator>
+  );
+}
+
 export default function AppNavigator() {
-  return <>AppNavigator</>;
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: true,
+        headerTitleAlign: "center",
+        headerStyle: { elevation: 0 },
+      }}
+    >
+      <Stack.Screen
+        name="HomeTabs"
+        component={HomeTabs}
+        options={{ headerShown: false }}
+      />
+
+      <Stack.Screen
+        name="Settings"
+        component={SettingsScreen}
+        options={{ title: "Settings" }}
+      />
+    </Stack.Navigator>
+  );
 }
