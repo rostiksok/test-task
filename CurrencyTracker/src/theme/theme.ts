e9fd592171328f61197ed943b065498e4e757311
Rostyslav Sokalskyi <rostyksokalskyi@gmail.com>
2025-06-24 20:11:31 +0300

    feat: add theme

---
+++ CurrencyTracker/src/theme/theme.ts
@@ -0,0 +1,37 @@
+export type ThemeType = "Light" | "Dark";
+
+export const COLORS = {
+  light: {
+    background1: "#ffffff",
+    background: "#f2f2f2",
+    card: "#ffffff",
+    text: "black",
+    primary: "#afc6ff",
+    secondary: "#ff8000",
+    border: "#E5E5E5",
+    placeholder: "#F8F8F8",
+  },
+  dark: {
+    background1: "#121212",
+    background: "#17181a",
+    card: "#2e3336",
+    text: "white",
+    primary: "#afc6ff",
+    secondary: "#ff9f0a",
+    border: "#3C3C3C",
+    placeholder: "#151515",
+  },
+};
+
+export const SIZES = {
+  padding: 16,
+  smallPadding: 8,
+  cardRadius: 8,
+  iconSize: 16,
+  font: {
+    h1: 24,
+    h2: 18,
+    body: 16,
+    caption: 12,
+  },
+};
