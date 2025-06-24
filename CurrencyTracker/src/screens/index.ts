483bc2fc74498d37c8131b2b65484f7953e923a5
Rostyslav Sokalskyi <rostyksokalskyi@gmail.com>
2025-06-24 20:02:46 +0300

    update: imports

---
+++ CurrencyTracker/src/screens/index.ts
@@ -0,0 +1,4 @@
+
+export { default as AllCurrenciesScreen } from "./AllСurrenciesScreen";
+export { default as FavoritesScreen } from "./FavoritesScreen";
+export { default as SettingsScreen } from "./SettingsScreen";
